e31d26b6dfe382794b5c4571f60337c5d36e83e5
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-02-09 10:05:20 +0100

    add relationship (profiles-user)

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 
+
 class Profile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id','curriculum','image','tel','visibility','address','services'
+    ];
+
+    public function user(){
+        return $this->belongTo(User::class);
+    }
 }

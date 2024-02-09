cb161c5f9b0819a4ce446585c21d15009510d3e8
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-02-09 16:32:48 +0100

    added vote seeder

--- database/migrations/2024_02_09_092445_create_specialties_table.php
+++ database/migrations/2024_02_09_092445_create_specialties_table.php
@@ -24,9 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::disableForeignKeyConstraints();
-
         Schema::dropIfExists('specialties');
         Schema::enableForeignKeyConstraints();
-
     }
 };
--- database/seeders/VoteSeeder.php
+++ database/seeders/VoteSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Vote;
+
 class VoteSeeder extends Seeder
 {
     /**
@@ -12,6 +14,10 @@ class VoteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for($i=1;$i<=10;$i++){
+            $newVote = new Vote();
+            $newVote->value = $i;
+            $newVote->save();
+        }
     }
 }

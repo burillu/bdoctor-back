4c3486b6a4ccfec40e2fd289e837dbd32971f1e3
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2024-02-09 17:31:17 +0100

    fix DatabaseSeeder call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             //ProfileSeeder::class,
-            SpecialtieSeeder::class,
+            SpecialtySeeder::class,
             SponsorshipSeeder::class,
             //ReviewSeeder::class,
             //VoteSeeder::class,

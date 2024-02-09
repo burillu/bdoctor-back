c19f1f3988644a346cf1371c906144b2b35963fc
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2024-02-09 11:00:58 +0100

    Add migrations for sponsorsip and profile_sponsorship

---
+++ app/Models/Sponsorship.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sponsorship extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name','price','duration'];
+    public function profiles()
+    {
+        return $this->belongsToMany(Profile::class);
+    }
+}

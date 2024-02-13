15e996797157995d12702ad6941ec3c54a15c630
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-02-13 16:11:27 +0100

    added error messages

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,12 +38,34 @@ class RegisteredUserController extends Controller
     {
         // dd($request);
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'alpha:ascii', 'max:255'],
+            'last_name' => ['required', 'alpha:ascii', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed','min:8', Rules\Password::defaults()],
+            'password_confirmation' => ['required', 'min:8','confirmed'],
             'specialties' => ['required', 'exists:specialties,id'],
         ], [
+            'name.required'=>'Questo campo è obbligatorio',
+            'name.alpha'=>'Questo campo deve essere testuale',
+            'name.max'=>'Questo può essere lungo massimo :max caratteri',
+            'last_name.required'=>'Questo campo è obbligatorio',
+            'last_name.alpha'=>'Questo campo deve essere testuale',
+            'last_name.max'=>'Questo può essere lungo massimo :max caratteri',
+            'email.required'=>'Questo campo è obbligatorio',
+            'email.string'=>'Questo campo deve essere una stringa di testo',
+            'email.max'=>'Questo può essere lungo massimo :max caratteri',
+            'email.lowercase'=>'Questo campo deve essere scritto in minuscolo',
+            'email.unique'=>'Questa email esiste già',
+            'address.required'=>'Questo campo è obbligatorio',
+            'address.string'=>'Questo campo deve essere una stringa di testo',
+            'address.max'=>'Questo può essere lungo massimo :max caratteri',
+            'password.required'=>'Questo campo è obbligatorio',
+            'password.min'=>'Questo campo deve essere lungo almeno :min caratteri',
+            'password.confirmed'=>'password non corrisponde alla conferma',
+            'password_confirmation.required'=>'questo campo è obbligatorio',
+            'password_confirmation.min'=>'Questo campo deve essere lungo almeno :min caratteri',
+            'password_confirmation.confirmed'=>'',
             'specialties.required' => 'Inserire almeno un valore.',
         ]);
 
@@ -56,7 +78,7 @@ class RegisteredUserController extends Controller
         if($user){
              $profile=Profile::create([
             'address' => $request->address,
-            'slug'=> Str::slug($request->name . '-' .$request->last_name, '-'),
+            // 'slug'=> Str::slug($request->name . '-' .$request->last_name, '-'),
             'user_id' => $user->id,
         ]);
         }

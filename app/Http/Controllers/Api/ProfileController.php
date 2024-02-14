de8702ea130683205e0211a5cc2d196c4e0448f9
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-02-14 12:28:01 +0100

    Doctors API algorithm improved

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -8,67 +8,71 @@ use App\Models\Profile;
 
 class ProfileController extends Controller
 {
-        //aggiungere specialties
-        //services, curriculum, id 
-        public function index(){
-            $doctors = Profile::with('user')->with('specialties')->get();
-            $data = $doctors->map(function ($doctor){
-                $specialties = [];
-                foreach($doctor->specialties as $specialty){
-                    array_push($specialties,$specialty->name);
-                }
-                return[
-                    'id' => $doctor->id,
-                    'name' => $doctor->user->name,
-                    'last_name'=> $doctor->user->last_name,
-                    'address'=> $doctor->address,
-                    'image' => $doctor->image,
-                    'tel' => $doctor->tel,
-                    'visibility' => $doctor->visibility,
-                    'slug' => $doctor->slug,
-                    'spelcieties' => $specialties,
-                ];
-            });
+    // Aggiungere specialties
+    // Services, curriculum, id 
+    public function index()
+    {
+        $doctors = Profile::with('user')->with('specialties')->get();
+        $data = $doctors->map(function ($doctor) {
+            $specialties = $this->specialtiesNames($doctor);
+            return [
+                'id' => $doctor->id,
+                'name' => $doctor->user->name,
+                'last_name' => $doctor->user->last_name,
+                'address' => $doctor->address,
+                'image' => $doctor->image,
+                'tel' => $doctor->tel,
+                'visibility' => $doctor->visibility,
+                'slug' => $doctor->slug,
+                'specialties' => $specialties,
+            ];
+        });
 
-            return response()->json(
-                [
-                    'success' => true,
-                    'results' => $data,
-                ]
-            );
-        }
+        return response()->json([
+            'success' => true,
+            'results' => $data,
+        ]);
+    }
 
-    public function show($slug){
+    public function show($slug)
+    {
         $doctor = Profile::with('user')->with('specialties')->where('slug', $slug)->first();
-        if($doctor) {
-            $specialties = [];
-            foreach($doctor->specialties as $specialty){
-                array_push($specialties,$specialty->name);
-            }
+        if ($doctor) {
+            $specialties = $this->specialtiesNames($doctor);
             $data = [
-                    'id' => $doctor->id,
-                    'name' => $doctor->user->name,
-                    'last_name'=> $doctor->user->last_name,
-                    'address'=> $doctor->address,
-                    'id' => $doctor->id,
-                    'curriculum' => $doctor->curriculum,
-                    'image' => $doctor->image,
-                    'tel' => $doctor->tel,
-                    'visibility' => $doctor->visibility,
-                    'services' => $doctor->services,
-                    'slug' => $doctor->slug,
-                    'spelcieties' => $specialties,
+                'id' => $doctor->id,
+                'name' => $doctor->user->name,
+                'last_name' => $doctor->user->last_name,
+                'address' => $doctor->address,
+                'curriculum' => $doctor->curriculum,
+                'image' => $doctor->image,
+                'tel' => $doctor->tel,
+                'visibility' => $doctor->visibility,
+                'services' => $doctor->services,
+                'slug' => $doctor->slug,
+                'specialties' => $specialties,
             ];
             return response()->json([
                 'success' => true,
                 'result' => $data,
             ]);
-
-        }else{
+        } else {
             return response()->json([
                 'success' => false,
-                'result' => 'dottore non trovato',
+                'result' => 'Dottore non trovato',
             ]);
         }
+    }
+
+    /**
+     * preso il parametro $doctor, cicla le specialties e ne prende solo in nome, e restituisce l'array pieno
+     */
+    public function specialtiesNames($doctor)
+    {
+        $specialties = [];
+        foreach ($doctor->specialties as $specialty) {
+            array_push($specialties, $specialty->name);
+        }
+        return $specialties;
     }
 }

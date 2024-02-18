5de3c6a992e808ef02eea676e79ea7cf588ddef6
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-02-18 10:36:22 +0100

    updated leads api store with the basic algoritm

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,10 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Lead;
 
 class LeadController extends Controller
 {
      public function store(Request $request){
+
+        $new_lead= new Lead();
+        $new_lead->profile_id = $request->profile_id;
+        $new_lead->name= $request->name;
+        $new_lead->surname = $request->surname;
+        $new_lead->email = $request->email;
+        $new_lead->tel = $request->tel ;
+        $new_lead->message = $request->message;
+        $new_lead->save();
+
        return response()->json([
         'success' => true,
         'message' => 'leads salvato',

a2043985415d4b8b026a688022153a4627cefe4b
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-02-18 12:16:24 +0100

    updated reviews api store with the advanced controls

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -11,7 +11,55 @@ use App\Models\Profile;
 class ReviewController extends Controller
 {
      public function store(Request $request){
-        
+
+        if(!Profile::where('id', $request->profile_id)->exists()){
+            return response()->json([
+                'success' => false,
+                'message' => "l'id del profilo inserito non esiste",
+                ]);
+        }
+        if($request->title && strlen($request->title) > 255){
+            return response()->json([
+                'success' => false,
+                'message' => 'il campo title se esiste deve essere inferiore a 255 caratteri',
+                ]);
+        }
+        if($request->body && strlen($request->body) > 65535){
+            return response()->json([
+                'success' => false,
+                'message' => 'il campo body deve essere inferiore a 65535 caratteri',
+                ]);
+        }
+        if(strlen($request->email) > 255){
+            return response()->json([
+                'success' => false,
+                'message' => 'il campo email deve essere inferiore a 255 caratteri',
+                ]);
+        }
+        if(!filter_var($request->email, FILTER_VALIDATE_EMAIL)){
+            return response()->json([
+                'success' => false,
+                'message' => 'il campo email deve essere una email valida',
+                ]);
+        }
+        if(strlen($request->name) > 255){
+            return response()->json([
+                'success' => false,
+                'message' => 'il campo name deve essere inferiore a 255 caratteri',
+                ]);
+        }
+        if(!preg_match('/^[a-zA-Z\s]+$/', $request->name)){
+            return response()->json([
+                'success' => false,
+                'message' => 'il campo name deve essere testuale',
+                ]);
+        }
+        if(!Vote::where('id', $request->vote_id)->exists()){
+            return response()->json([
+                'success' => false,
+                'message' => "l'id del voto inserito non esiste",
+                ]);
+        }
         $new_review= new Review();
         $new_review->profile_id = $request->profile_id;
         if($request->title)$new_review->title= $request->title;
@@ -21,7 +69,7 @@ class ReviewController extends Controller
         $new_review->save();
 
         $profile_vote = Profile::find($request->profile_id);
-        $profile_vote->votes()->attach($request->vote);
+        $profile_vote->votes()->attach($request->vote_id);
 
         return response()->json([
             'success' => true,

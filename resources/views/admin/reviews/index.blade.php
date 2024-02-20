063b4337ad0e34646f7b6db92ca31867ebd0e275
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2024-02-20 15:51:16 +0100

    Remove message on messages and reviews tables

--- resources/views/admin/reviews/index.blade.php
+++ resources/views/admin/reviews/index.blade.php
@@ -5,11 +5,11 @@
         <table class="table table-hover">
             <thead>
                 <tr class="text-center">
-                    <th scope="col">#</th>
+                    {{-- <th scope="col">#</th> --}}
                     <th scope="col">Nome</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Titolo</th>
-                    <th scope="col">Anteprima</th>
+{{--                     <th scope="col">Titolo</th>
+                    <th scope="col">Anteprima</th> --}}
                     <th scope="col">Data/ora</th>
                     <th scope="col">Azioni</th>
                 </tr>
@@ -17,11 +17,11 @@
             <tbody>
                 @foreach ($reviews as $review)
                     <tr class="text-center">
-                        <td>{{ $loop->iteration }}</td>
+                        {{-- <td>{{ $loop->iteration }}</td> --}}
                         <td>{{ $review->name }}</td>
                         <td>{{ $review->email }}</td>
-                        <td>{{ $review->title }} </td>
-                        <td>{{ strlen($review->body) > 80 ? substr($review->body, 0, 80) . '...' : $review->body }}</td>
+{{--                         <td>{{ $review->title }} </td>
+                        <td>{{ strlen($review->body) > 80 ? substr($review->body, 0, 80) . '...' : $review->body }}</td> --}}
                         <td class="col-3">{{ date('d/m/Y \O\r\e\: H:i', strtotime($review->created_at)) }} </td>
                         <td class="">
                             <a class="btn btn-primary" href=" {{ route('admin.reviews.show', $review->id) }}"><i

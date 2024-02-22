3c2c515a62510842c59a677f37b91e36be6ffa04
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-02-22 11:51:12 +0100

    added statistics for messages by month

--- app/Http/Controllers/Admin/StatsController.php
+++ app/Http/Controllers/Admin/StatsController.php
@@ -2,19 +2,61 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Sponsorship;
+use App\Models\User;
+use App\Models\Profile;
+use App\Models\Vote;
+use App\Models\Review;
+use App\Models\Lead;
 use Braintree\Gateway;
 
 class StatsController extends Controller
 {
     public function index()
     {
+        //per sponsorships
         $sponsorships = Sponsorship::all();
         $gateway = new Gateway(config('services.braintree'));
         $clientToken = $gateway->clientToken()->generate();
 
-        return view('admin.stats.index', compact('clientToken','sponsorships'));
+        $user = Auth::user();
+        $profile = $user->profile;
+
+        //per grafico numero di messaggi ricevuti di ogni mese
+        $currentYear = now()->year;
+        $currentMonth = now()->month;
+        //CREARE FUNZIONE PER MESE A ANNO INSERITO
+        
+        $messagesByMonth = Lead::selectRaw('MONTH(created_at) as month, COUNT(*) as message_count')
+    ->where('profile_id', $profile->id)
+    ->groupByRaw('MONTH(created_at)')
+    ->get();
+
+    $leads = [
+        'January' => 0,
+        'February' => 0,
+        'March' => 0,
+        'April' => 0,
+        'May' => 0,
+        'June' => 0,
+        'July' => 0,
+        'August' => 0,
+        'September' => 0,
+        'October' => 0,
+        'November' => 0,
+        'December' => 0,
+    ];
+
+    foreach ($messagesByMonth as $message) {
+        $monthName = date('F', mktime(0, 0, 0, $message->month, 1)); 
+        $leads[$monthName] = $message->message_count; 
+    }       
+    // $leads['July'] = 10;
+    // dd($leads);
+
+        return view('admin.stats.index', compact('clientToken','sponsorships','leads'));
     }
 }
--- resources/views/admin/stats/index.blade.php
+++ resources/views/admin/stats/index.blade.php
@@ -1,4 +1,44 @@
 @extends('admin.dashboard')
 @section('dashboard_content')
+
+
     <h1>Statistiche</h1>
+    <div>
+        <canvas id="ChartLeads"></canvas>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+    const ctx = document.getElementById('ChartLeads');
+    
+    let leads = [];
+    let count = 0;
+    @foreach ($leads as $value)
+        leads[count] = {{ $value }};
+        count++;
+    @endforeach
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+        datasets: [{
+            label: 'messaggi per mese',
+            data: leads,
+            borderWidth: 1,
+            hoverBorderWidth: 2,
+        }]
+        },
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
+        }
+        }
+    });
+    </script>
+
+    
 @endsection
